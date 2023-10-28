f2e728574a6d355e7b8dc1c9f7f2d777cea4f9e8
Remi Salmon <salmon.remi@gmail.com>
2023-10-27 21:20:10 -0600

    Add pull request checks

--- test.sh
+++ test.sh
@@ -7,4 +7,4 @@ source venv/bin/activate
 python -m pip install -U pip setuptools
 python -m pip install -r requirements.txt
 
-python -m doctest -o IGNORE_EXCEPTION_DETAIL -f tests/tests.txt || exit 1
+python -m doctest -o IGNORE_EXCEPTION_DETAIL -f tests/tests.txt
